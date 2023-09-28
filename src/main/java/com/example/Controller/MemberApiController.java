919da4b8840c72c048427d71ddbd1122e28d6b99
harper.wen <owilalala974@gmail.com>
2023-09-28 17:48:46 +0800

    feat: new java spring boot project for practise

---
+++ src/main/java/com/example/Controller/MemberApiController.java
@@ -0,0 +1,26 @@
+package com.example.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Model.MemberAccount;
+
+@RestController
+public class MemberApiController {
+    @Autowired
+    MemberAccount memberAccount;
+
+    @GetMapping("/memberApi/memberTest")
+    //TODO: 失敗~
+    public MemberAccount memberTest() {
+        MemberAccount memberAccount = new MemberAccount();
+        memberAccount.setAddress("台北市");
+        memberAccount.setCellphone("0912345678");
+        memberAccount.setEmail("test@example.com");
+        memberAccount.setId(1);
+        memberAccount.setPassword("12345");
+        return memberAccount;
+    }
+}
